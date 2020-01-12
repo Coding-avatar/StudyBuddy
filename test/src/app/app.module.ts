787c53b683e598ac6d7dd68b2c86a120f9dc33c4
Md Nife Salil Islam <nifesalilislam@gmail.com>
2020-01-13 04:23:51 +0530

    components generated for testing

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostquestionComponent } from './postquestion/postquestion.component';
+import { DisplayquestionComponent } from './displayquestion/displayquestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostquestionComponent,
+    DisplayquestionComponent
   ],
   imports: [
     BrowserModule,
